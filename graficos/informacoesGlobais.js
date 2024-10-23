b7006dfb1eef6f63cc1253f121b18603a73430bd
jujucastelli <130070249+jujucastelli@users.noreply.github.com>
2024-10-23 07:46:23 -0300

    Update informacoesGlobais.js

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -1,1 +1,9 @@
+const url= "";
 
+async function visualizarinformacoesGlobais() {
+  const res = await fetch(url)
+  const dados = await res.json()
+  console.log(dados);
+}
+
+visualizarinformacoesGlobais();
